050563c0e904cb1e59f21b1bf05a1b16ca687500
suman <suman.bhattarai@gmail.com>
2021-06-19 18:48:56 +0545

    count frequency

--- 1-character-frequency-count.js
+++ 1-character-frequency-count.js
@@ -13,3 +13,20 @@ function charCount(str) {
 
 var out = charCount("Data structure and algorithm 101");
 console.log(out);
+
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+### Data structure and Algorithm in JavaScript 
+- 1-problem-solving-patterns
+  - 1-same-frequency-in-two-array.js
+- 1-character-frequency-count.js
