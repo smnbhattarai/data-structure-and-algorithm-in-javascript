33c5ce93ac725cd0505dd3b394470edf1c445a34
suman <suman.bhattarai@gmail.com>
2021-07-06 22:04:24 +0545

    dijkstra

---
+++ 10-graphs/4-dijkstra.js
@@ -0,0 +1,98 @@
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2, weight) {
+        this.adjacencyList[v1].push({node: v2, weight: weight});
+        this.adjacencyList[v2].push({node: v1, weight: weight});
+    }
+
+    dijkstra(start, finish) {
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        const path = [];
+        let smallest;
+
+        // build initial state
+        for (let vertex in this.adjacencyList) {
+            if (vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+
+        while (nodes.values.length) {
+            smallest = nodes.dequeue().val;
+            if (smallest === finish) {
+                while (previous[smallest]) {
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+            if (smallest || distances[smallest] !== Infinity) {
+                for (let neighbor in this.adjacencyList[smallest]) {
+                    let nextNode = this.adjacencyList[smallest][neighbor];
+                    let candidate = distances[smallest] + nextNode.weight;
+                    let nextNeighbor = nextNode.node;
+                    if (candidate < distances[nextNeighbor]) {
+                        distances[nextNeighbor] = candidate;
+                        previous[nextNeighbor] = smallest;
+                        nodes.enqueue(nextNeighbor, candidate);
+                    }
+                }
+            }
+        }
+        return path.concat(smallest).reverse();
+    }
+
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        this.values.push({val, priority});
+        this.sort();
+    }
+
+    dequeue() {
+        return this.values.shift();
+    }
+
+    sort() {
+        this.values.sort((a, b) => a.priority - b.priority);
+    }
+}
+
+let wg = new WeightedGraph();
+wg.addVertex("A");
+wg.addVertex("B");
+wg.addVertex("C");
+wg.addVertex("D");
+wg.addVertex("E");
+wg.addVertex("F");
+wg.addEdge("A", "B", 4);
+wg.addEdge("A", "C", 2);
+wg.addEdge("B", "E", 3);
+wg.addEdge("C", "D", 2);
+wg.addEdge("C", "F", 4);
+wg.addEdge("D", "E", 3);
+wg.addEdge("D", "F", 1);
+wg.addEdge("E", "F", 1);
+console.log(wg);
+console.log(wg.dijkstra("A", "E"));
+console.log(wg.dijkstra("A", "F"));
+console.log(wg.dijkstra("E", "A"));
